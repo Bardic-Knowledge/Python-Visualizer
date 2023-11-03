48afc530a2c105acef76b562956aa479a9faa34c
Roger DiMaggio <dimaggioroger@gmail.com>
2023-11-03 11:40:56 -0700

    update to include a mandelbrot

--- import pyaudio.py
+++ import pyaudio.py
@@ -17,28 +17,63 @@ stream = p.open(format=FORMAT,
                 input=True,
                 frames_per_buffer=CHUNK)
 
-# Create a function to update the audio display
-def update_audio_display():
+def mandelbrot(c, max_iter):
+    z = c
+    for n in range(max_iter):
+        if abs(z) > 2:
+            return n
+        z = z * z + c
+    return max_iter
+
+def generate_mandelbrot(width, height, x_min, x_max, y_min, y_max, max_iter):
+    x = np.linspace(x_min, x_max, width)
+    y = np.linspace(y_min, y_max, height)
+    mandelbrot_set = np.zeros((width, height), dtype=int)
+
+    for i in range(width):
+        for j in range(height):
+            real = x[i]
+            imag = y[j]
+            c = complex(real, imag)
+            mandelbrot_set[i, j] = mandelbrot(c, max_iter)
+
+    return mandelbrot_set
+
+# Create a function to update the Mandelbrot display based on audio data
+def update_mandelbrot_display():
     data = stream.read(CHUNK)
     audio_data = np.frombuffer(data, dtype=np.int16)
     
-    # Process audio_data (in this case, plot the waveform)
+    # Process audio_data and use it to modify Mandelbrot parameters
+    audio_amplitude = np.max(np.abs(audio_data))  # Measure the audio amplitude
+    zoom = 1.0 * audio_amplitude / 500.0  # Modify zoom based on audio
+    
+    # Generate and plot the Mandelbrot set with the modified parameters
+    x_min, x_max = -2 / zoom, 1 / zoom
+    y_min, y_max = -1 / zoom, 1 / zoom
+    mandelbrot_set = generate_mandelbrot(width, height, x_min, x_max, y_min, y_max, max_iter)
+    
     plt.clf()
-    plt.plot(audio_data)
-    plt.title("Audio Waveform")
-    plt.xlabel("Sample")
-    plt.ylabel("Amplitude")
-    plt.pause(0.1)
+    plt.imshow(mandelbrot_set.T, extent=(x_min, x_max, y_min, y_max))
+    #plt.xlabel("Real")
+    #plt.ylabel("Imaginary")
+    #plt.title("Mandelbrot Set")
+    plt.pause(0.50)
     
     # Schedule the function to run again
-    root.after(10, update_audio_display)
+    root.after(10, update_mandelbrot_display)
 
 # Create a new window
 root = tk.Tk()
-root.title("Audio Capture and Display")
+root.title("Audio-Influenced Mandelbrot Viewer")
 
-# Start updating the audio display
-update_audio_display()
+# Mandelbrot parameters
+width = 800
+height = 800
+max_iter = 16
+
+# Start updating the Mandelbrot display
+update_mandelbrot_display()
 
 # Start the main event loop
 root.mainloop()
